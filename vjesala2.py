bd535ca0008a3672e2e52111ddc339fc0e66ebdc
atrconic <ana.trconic@gmail.com>
2023-01-14 19:22:18 +0100

    add session log in log out

--- vjesala2.py
+++ vjesala2.py
@@ -1,7 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask import session
 
 app = Flask(__name__)
+app.secret_key = b'314159'
+
 
 @app.route("/")
 def hello_world():
-    return render_template('index2.html')
+    username = session.get("username")
+    return render_template('index2.html', username=username)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('hello_world'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    if 'username' in session:
+        session.pop('username', None)
+    return redirect(url_for('hello_world'))
+
